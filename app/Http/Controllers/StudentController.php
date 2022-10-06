f96ddcb894ebaabc936e636fb932f67295c26efa
Christopher Panerio <christopherpaneriotup@gmail.com>
2022-10-06 13:26:53 +0800

    Web Application

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Student;
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $student = Student::latest()->paginate(5);
+        return response()->json($student, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'first_name' =>'required',
+            'last_name' =>'required',
+            'age' =>'required',
+        ]);
+
+        $student = Student::create($request->all());
+        return response()->json($student, 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Student  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Student $student)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Student  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Student $student)
+    {
+        $request->validate([
+            'first_name' =>'required',
+            'last_name' =>'required',
+            'age' =>'required',
+        ]);
+
+        $student->update($request->all());
+        return response()->json($student, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Student  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Student $student)
+    {
+        $student->delete();
+        return response()->json('Record Deleted Successfully');
+    }
+}
